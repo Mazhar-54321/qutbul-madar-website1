d34eee1ee9adb8044b24faef425b950772fab838
Mazhar-LifeMine <mazarali@lifemine.io>
2026-02-15 18:07:47 +0530

    next config settings

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig = {
-  // Your other config (e.g. images, etc.)
+  webpack: (config: { resolve: { fallback: any } }, { isServer }: any) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default withNextIntl(nextConfig);
