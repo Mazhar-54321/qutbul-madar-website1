0c1a873d946db308a8d54a4772a18ab78039460b
Mazhar-LifeMine <mazarali@lifemine.io>
2026-02-15 18:16:38 +0530

    next config ts

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,8 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig = {
-  webpack: (config: { resolve: { fallback: any } }, { isServer }: any) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-    }
-    return config;
+  experimental: {
+    esmExternals: true,
   },
 };
 
